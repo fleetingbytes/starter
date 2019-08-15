40683287108688e8ab2669a8fc9d8d6d3f4a3505
Nagidal <nagidal146@gmail.com>
2019-08-15 23:08:52 +0200

    fixed bug with no config file

--- project.py
+++ project.py
@@ -9,6 +9,12 @@ logging.config.dictConfig(logging_conf.dict_config)
 logger = logging.getLogger(__name__)
 
 
+class Config():
+    def __init__(self, config: configparser.ConfigParser) -> None:
+        self.config = config
+        self.path_to_data = pathlib.Path(self.config.get("Paths", "path_to_data").strip('"'))
+
+
 def read_config_file(path_to_config_file: pathlib.Path, cfgparser: configparser.ConfigParser) -> configparser.ConfigParser:
     """
     Reads current configuration file or creates a new one with a default configuration
@@ -19,13 +25,14 @@ def read_config_file(path_to_config_file: pathlib.Path, cfgparser: configparser.
         return cfgparser
     except FileNotFoundError:
         logger.info("config file missing, creating a new one")
-        create_config_file(path_to_config_file, cfgparser)
+        return create_config_file(path_to_config_file, cfgparser)
 
 
 def create_config_file(path_to_config_file: pathlib.Path, cfgparser: configparser.ConfigParser) -> configparser.ConfigParser:
     logger.debug("creating cfg")
     cfgparser.add_section("Paths")
-    cfgparser.set("Paths", f"# Set the absolute paths. They must be in quotes, like this: \"{pathlib.Path.cwd()}\"")
+    cfgparser.set("Paths", f"# Set the absolute paths.")
+    cfgparser.set("Paths", "path_to_data", f"{pathlib.Path.cwd()}")
     with open(path_to_config_file, mode="w", encoding="utf-8") as configfh:
         cfgparser.write(configfh)
     return read_config_file(path_to_config_file, cfgparser)
@@ -44,8 +51,10 @@ def debug_delete_config(path_to_configfile: pathlib.Path) -> None:
 if __name__ == "__main__":
     logger.debug("Program started")
     path_to_config_file = pathlib.Path("config.ini")
-    config = configparser.ConfigParser(allow_no_value=True)
-    config.optionxform = str
-    config = read_config_file(pathlib.Path(path_to_config_file), config)
+    config_parser = configparser.ConfigParser(allow_no_value=True)
+    config_parser.optionxform = str
+    parsed_config = read_config_file(pathlib.Path(path_to_config_file), config_parser)
+    config = Config(parsed_config)
+    logger.info(f"Path to data: {repr(config.path_to_data)}")
     debug_delete_config(path_to_config_file)
     logger.debug("Program ended")
